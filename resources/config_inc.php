fdd8f19cc9cf7a51a87c53e1fcae71ee6bd94ba0
baykov-sv <baykov-sv@yandex.ru>
2022-01-16 20:15:56 +0300

    FTP

---
+++ resources/config_inc.php
@@ -0,0 +1,15 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Moscow';
+
+$g_crypto_master_salt     = 'p4e3hIC5lPN/Rfh3pJCaXEjUrjEuyaK/zgae/tEvb/8=';
+
+$g_default_language = 'english';
+
+$g_signup_use_captcha = OFF;
+
